6806d282e20bfea433f5aa38e101157936fa2c2c
wajeedd <153074817+wajeedd@users.noreply.github.com>
2026-03-07 10:35:52 +0530

    Time: 48 ms (49.92%), Space: 39.4 MB (29.5%) - LeetHub

--- 0015-3sum/0015-3sum.c
+++ 0015-3sum/0015-3sum.c
@@ -4,22 +4,8 @@ int cmp(const void *a, const void *b){
     return (*(int*)a - *(int*)b);
 }
 
-int binarySearch(int* nums, int left, int right, int target){
-    while(left <= right){
-        int mid = left + (right - left)/2;
-
-        if(nums[mid] == target)
-            return mid;
-        else if(nums[mid] < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return -1;
-}
-
 int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-
+    
     qsort(nums, numsSize, sizeof(int), cmp);
 
     int capacity = numsSize * numsSize;
@@ -28,29 +14,43 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
 
     *returnSize = 0;
 
-    for(int i = 0; i < numsSize - 2; i++){
+    for(int i = 0; i < numsSize - 2; i++) {
 
         if(i > 0 && nums[i] == nums[i-1])
             continue;
 
-        for(int j = i + 1; j < numsSize - 1; j++){
+        int left = i + 1;
+        int right = numsSize - 1;
 
-            if(j > i + 1 && nums[j] == nums[j-1])
-                continue;
+        while(left < right) {
 
-            int target = -(nums[i] + nums[j]);
+            int sum = nums[i] + nums[left] + nums[right];
 
-            int k = binarySearch(nums, j+1, numsSize-1, target);
+            if(sum == 0) {
 
-            if(k != -1){
                 res[*returnSize] = malloc(3 * sizeof(int));
                 res[*returnSize][0] = nums[i];
-                res[*returnSize][1] = nums[j];
-                res[*returnSize][2] = nums[k];
+                res[*returnSize][1] = nums[left];
+                res[*returnSize][2] = nums[right];
 
                 (*returnColumnSizes)[*returnSize] = 3;
                 (*returnSize)++;
+
+                while(left < right && nums[left] == nums[left+1])
+                    left++;
+
+                while(left < right && nums[right] == nums[right-1])
+                    right--;
+
+                left++;
+                right--;
             }
+
+            else if(sum < 0)
+                left++;
+
+            else
+                right--;
         }
     }
 
