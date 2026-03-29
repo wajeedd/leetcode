602b442a68c8d9a4a38096d80714dca6da192d4d
wajeedd <153074817+wajeedd@users.noreply.github.com>
2026-03-30 00:19:30 +0530

    Time: 0 ms (100%), Space: 12.3 MB (74.07%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def postorderTraversal(self, root):
+        if not root:
+            return []
+        
+        stack = [root]
+        result = []
+        
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        
+        return result[::-1]  
